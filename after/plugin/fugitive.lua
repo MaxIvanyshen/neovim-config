202a7b804c754d52072771ffacb1b31ecab9d7f7
MaxIvanyshen <maxivanyshen@gmail.com>
2025-01-03 00:50:03 +0200

    feat: add more hotkeys for fugitive

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -3,3 +3,5 @@ vim.keymap.set("n", "<leader>gaa", '<cmd>Git add .<CR>');
 vim.keymap.set("n", "<leader>gc", '<cmd>Git commit <CR>');
 vim.keymap.set("n", "<leader>gp", '<cmd>Git push<CR>');
 vim.keymap.set("n", "<leader>gs", '<cmd>Git<CR>');
+vim.keymap.set("n", "<leader>dt", '<cmd>Git difftool<CR>');
+vim.keymap.set("n", "<leader>mt", '<cmd>Git mergetool<CR>');
