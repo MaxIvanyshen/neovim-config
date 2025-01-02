643ecb5f242ff1dcd042fbe176095e26932e0f89
MaxIvanyshen <maxivanyshen@gmail.com>
2025-01-03 00:51:49 +0200

    feat: add hotkey to close quickfix list

--- lua/frozr/remap.lua
+++ lua/frozr/remap.lua
@@ -10,8 +10,10 @@ vim.keymap.set("n", "<leader>h", '<cmd>cprev<CR>zz'); -- go to next item in quic
 
 vim.keymap.set("n", "<leader>]", '<cmd>cnewer<CR>zz'); -- go to next quickfix list
 vim.keymap.set("n", "<leader>[", '<cmd>colder<CR>zz'); -- go to previous quickfix list
+vim.keymap.set("n", "<leader>qc", "<cmd>cclose<CR>");
 
 vim.keymap.set("n", "<leader>sf", 'yiw <cmd>execute \'vimgrep /\' . escape(@", \'/\') . \'/ %\'<CR>zz'); -- search for yanked word and move everything to quickfix list
 
 -- Some useful stuff
 vim.keymap.set("n", "<leader>vs", '<cmd>vsplit<CR><C-w>l'); -- split screen vertically and move to right split
+
