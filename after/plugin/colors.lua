00505f0e6b7a3f404e1f15dd713d25e80a48f2a0
MaxIvanyshen <maxivanyshen@gmail.com>
2025-01-30 23:45:20 +0200

    delete unneeded config

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,12 +2,6 @@
 function Color(color)
     color = color or "tokyonight-moon"
     vim.cmd.colorscheme(color)
-
-    -- Enable for transparent background
-    --vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
-    --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
-    --vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none"})
-    --vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none"})
 end
 
 Color()
